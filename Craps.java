e5047e5627ba4d950fa2930643882965c89b4ac9
Ava Miralles <amiralles19@students.polytechnic.org>
2019-01-18 12:54:50 -0800

    First commit

---
+++ Craps.java
@@ -0,0 +1,33 @@
+
+/**
+ * Mimicks a game of craps
+ *
+ * @author Ava Miralles
+ * @version 01-18-2019
+ */
+public class Craps
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Craps
+     */
+    public Craps()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
